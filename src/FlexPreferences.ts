cf5f57d9641458f7d36159ecb5424dcca39887fa
Viraj Shah <shahvirajd@gmail.com>
2021-09-08 18:35:23 -0400

    Added titlebar previewer

--- src/FlexPreferences.ts
+++ src/FlexPreferences.ts
@@ -1,10 +1,11 @@
-import { changeTheme, HColor, HumanColorName } from '@Hi/Colors';
+import { changeTheme, HColor, HumanColorName, whichTheme } from '@Hi/Colors';
 import ClickButton from '@Hi/Components/ClickButton';
 import HIFullScreenView from '@Hi/Components/HIFullScreenView';
 import HStack from '@Hi/Components/HStack';
 import ImageView from '@Hi/Components/ImageView';
 import IonIcon from '@Hi/Components/IonIcon';
 import Spacer from '@Hi/Components/Spacer';
+import TextField from '@Hi/Components/TextField';
 import TextView from '@Hi/Components/TextView';
 import VStack from '@Hi/Components/VStack';
 import { ViewController } from '@Hi/ViewController';
@@ -56,8 +57,50 @@ function BrowserFramePreferences(): VStack {
                 .margin({ bottom: 10 }),
             new Spacer(),
             new ClickButton(new TextView('Edit')).padding(0)
+        ).stretchWidth(),
+
+        BrowserFramePreview()
+    )
+        .stretchWidth()
+        .padding();
+}
+
+function BrowserFramePreview(): VStack {
+    return new VStack(
+        new HStack(
+            new IonIcon('ellipse').foreground(HColor('red')),
+            new IonIcon('ellipse').foreground(HColor('orange')),
+            new IonIcon('ellipse').foreground(HColor('green')),
+            new Spacer()
+        ).stretchWidth(),
+        new HStack(
+            new HStack(
+                new IonIcon('chevron-back-circle-outline').font('xl'),
+                new IonIcon('chevron-forward-circle-outline').font('xl'),
+                new Spacer()
+            )
+                .width('20%')
+                .padding(5),
+            new Spacer(),
+            new HStack(new TextField('Search or Go to URL').stretchWidth())
+                .width('60%')
+                .padding(5),
+            new Spacer(),
+            new HStack(
+                new IonIcon('refresh-circle-outline').font('xl'),
+                new Spacer(),
+                new IonIcon('add-circle-outline').font('xl')
+            )
+                .width('20%')
+                .padding(5)
         ).stretchWidth()
-    ).stretchWidth();
+    )
+        .background(HColor('gray5'))
+        .foreground(BrowserPreferences.getPrimaryColor())
+        .stretchWidth()
+        .padding(5)
+        .rounded()
+        .rounded({ bottom: { left: 0, right: 0 } });
 }
 
 function HighlightColorPreferences(): VStack {
