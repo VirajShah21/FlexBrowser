7b36502446089458e8cf5869f032abe0398003bc
Viraj Shah <shahvirajd@gmail.com>
2021-09-08 23:12:03 -0400

    Updated some browser frame elements

--- src/FlexBrowserWindow.ts
+++ src/FlexBrowserWindow.ts
@@ -36,42 +36,47 @@ export default class FlexBrowserWindow extends HIFullScreenView {
                         .width('25%')
                         .padding({ left: 10, right: 10 }),
 
-                    new TextField('flex://home')
-                        .width({ min: 200, default: '50%', max: 600 })
-                        .textCenter()
-                        .id('url')
-                        .whenChanged(ev => {
-                            const browserWindow = ev.view.root(
-                                view =>
-                                    (view as FlexBrowserWindow).isBrowserWindow
-                            ) as FlexBrowserWindow;
-                            const icon = browserWindow.getViewById(
-                                'url-refresh-button'
-                            ) as IonIcon;
-
-                            (icon.body as HTMLInputElement).name =
-                                'arrow-forward-outline'; // ! Workaround to use .name
-                        })
-                        .noOutline()
-                        .whenFocused(ev =>
-                            ev.view.background(HColor('background')).textStart()
-                        )
-                        .whenUnfocused(ev =>
-                            ev.view.background('none').textCenter()
-                        )
-                        .whenKeyPressed(ev => {
-                            if (ev.key == 'Enter') {
+                    new HStack(
+                        new TextField('flex://home')
+                            .stretchWidth()
+                            .textCenter()
+                            .id('url')
+                            .whenChanged(ev => {
                                 const browserWindow = ev.view.root(
                                     view =>
                                         (view as FlexBrowserWindow)
                                             .isBrowserWindow
                                 ) as FlexBrowserWindow;
-                                const searchbar = this.getViewById(
-                                    'url'
-                                ) as InputField;
-                                browserWindow.goTo(searchbar.model.value);
-                            }
-                        }),
+                                const icon = browserWindow.getViewById(
+                                    'url-refresh-button'
+                                ) as IonIcon;
+
+                                (icon.body as HTMLInputElement).name =
+                                    'arrow-forward-outline'; // ! Workaround to use .name
+                            })
+                            .noOutline()
+                            .whenFocused(ev =>
+                                ev.view
+                                    .background(HColor('background'))
+                                    .textStart()
+                            )
+                            .whenUnfocused(ev =>
+                                ev.view.background('none').textCenter()
+                            )
+                            .whenKeyPressed(ev => {
+                                if (ev.key == 'Enter') {
+                                    const browserWindow = ev.view.root(
+                                        view =>
+                                            (view as FlexBrowserWindow)
+                                                .isBrowserWindow
+                                    ) as FlexBrowserWindow;
+                                    const searchbar = this.getViewById(
+                                        'url'
+                                    ) as InputField;
+                                    browserWindow.goTo(searchbar.model.value);
+                                }
+                            })
+                    ).width({ min: 200, default: '50%', max: 600 }),
 
                     new HStack(
                         TaskbarButton(
