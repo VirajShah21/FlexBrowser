58399b341f66a4573286a37f4c6913f5a898a2fd
Viraj Shah <shahvirajd@gmail.com>
2021-09-13 23:50:57 -0400

    Fix: Import was pointing to output ts file

--- src/components/RefreshTaskbarButton.ts
+++ src/components/RefreshTaskbarButton.ts
@@ -1,7 +1,7 @@
 import InputField from '@Hi/Components/InputField';
 import IonIcon from '@Hi/Components/IonIcon';
-import TaskbarButton from 'out/flex-browser-darwin-x64/flex-browser.app/Contents/Resources/app/src/components/TaskbarButton';
 import FlexBrowserWindow from 'src/FlexBrowserWindow';
+import TaskbarButton from './TaskbarButton';
 
 export default class RefreshTaskbarButton extends TaskbarButton {
     constructor() {
