6f93f5368b17eaaee76cd1b718ee57c096e9fd60
Viraj Shah <shahvirajd@gmail.com>
2021-09-14 21:02:18 -0400

    Fix: Using class component for add button

--- src/FlexHub/BrowserFrameComposer/PartitionComponentOrganizer.ts
+++ src/FlexHub/BrowserFrameComposer/PartitionComponentOrganizer.ts
@@ -1,15 +1,10 @@
 import { HColor } from '@Hi/Colors';
-import ClickButton from '@Hi/Components/ClickButton';
 import HStack from '@Hi/Components/HStack';
 import IonIcon from '@Hi/Components/IonIcon';
-import Overlay from '@Hi/Components/Overlay';
-import Spacer from '@Hi/Components/Spacer';
-import TextView from '@Hi/Components/TextView';
-import VStack from '@Hi/Components/VStack';
-import BrowserFrameModel from 'src/Models/BrowserFrameModel';
 import BrowserFrameRenderer from 'src/BrowserFrameRenderer';
 import BrowserPreferences from 'src/BrowserPreferences';
-import WidgetSelectorButton from './components/WidgetSelectorButton';
+import BrowserFrameModel from 'src/Models/BrowserFrameModel';
+import AddWidgetButton from './components/AddWidgetButton';
 
 export default class PartitionComponentOrganizer extends BrowserFrameRenderer {
     constructor(model: BrowserFrameModel) {
@@ -23,7 +18,7 @@ export default class PartitionComponentOrganizer extends BrowserFrameRenderer {
             ...this.model.partitions.map(partition =>
                 new HStack(
                     ...(partition.components.length == 0
-                        ? [AddWidgetButton()]
+                        ? [new AddWidgetButton()]
                         : partition.components.map(component =>
                               new IonIcon(component.icon || '').foreground(
                                   BrowserPreferences.getPrimaryColor()
@@ -33,45 +28,4 @@ export default class PartitionComponentOrganizer extends BrowserFrameRenderer {
             )
         );
     }
-}
-
-function AddWidgetButton(): ClickButton {
-    return new ClickButton(new IonIcon('hammer'))
-        .foreground(BrowserPreferences.getPrimaryColor())
-        .whenClicked(() => {
-            new Overlay(
-                new VStack(
-                    new Spacer(),
-
-                    new HStack(
-                        new WidgetSelectorButton(
-                            'Back Page',
-                            new IonIcon('chevron-back-circle-outline')
-                        ),
-                        new WidgetSelectorButton(
-                            'Forward Page',
-                            new IonIcon('chevron-forward-circle-outline')
-                        ),
-                        new WidgetSelectorButton(
-                            'New Window',
-                            new IonIcon('add-circle-outline')
-                        )
-                    ),
-
-                    new Spacer(),
-
-                    new ClickButton(new TextView('Cancel'))
-                        .background(HColor('red'))
-                        .foreground(HColor('background'))
-                        .rounded(7.5)
-                        .whenClicked(ev => {
-                            ev.view.root().destroy();
-                        }),
-
-                    new Spacer()
-                ).stretch()
-            )
-                .background(HColor('gray5'))
-                .foreground(HColor('foreground'));
-        });
 }
