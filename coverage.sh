501114fe671a1661b90c335a4b6aa7b567de8d86
Viraj Shah <shahvirajd@gmail.com>
2021-09-22 16:26:37 -0400

    Implemented coverage utils for github CI

---
+++ coverage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf coverage
+
+
+mkdir coverage
+mkdir coverage/src
+
+
+node_modules/.bin/jscover src coverage/src
+JSCOV=1 npx mocha -R mocha-lcov-reporter --recursive > coverage/coverage.lcov
