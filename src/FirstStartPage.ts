03d013ee8fa101047ab1c3851164fae3824e608c
Viraj Shah <shahvirajd@gmail.com>
2021-09-23 15:01:19 -0400

    Added color customization to the first start page

--- src/FirstStartPage.ts
+++ src/FirstStartPage.ts
@@ -6,6 +6,8 @@ import Spacer from '@Hi/Components/Spacer';
 import TextView, { FontWeight } from '@Hi/Components/TextView';
 import VStack from '@Hi/Components/VStack';
 import { ViewController } from '@Hi/ViewController';
+import HighlightColorPreferences from './FlexHub/components/HighlightColorPreferences';
+import ThemePreferences from './FlexHub/components/ThemePreferences';
 
 function MainIntro() {
     return new VStack(
@@ -18,11 +20,30 @@ function MainIntro() {
             FontWeight.Light,
         ),
         new Spacer(),
-    );
+    ).stretch();
+}
+
+function Theming() {
+    return new VStack(
+        new Spacer(),
+        new TextView('Customize Your Colors')
+            .font('xxl')
+            .weight(FontWeight.UltraLight)
+            .margin({ bottom: 25 }),
+        new TextView(
+            'Just one way to customize your browsing experience!',
+        ).weight(FontWeight.Light),
+        new ThemePreferences(),
+        new Spacer(),
+        new HighlightColorPreferences(),
+        new Spacer(),
+    ).stretch();
 }
 
 export default class FirstStartPage extends HIFullScreenView {
-    private controller: ViewController;
+    private readonly controller: ViewController;
+
+    private pageNumber: number;
 
     constructor() {
         super(
@@ -30,9 +51,13 @@ export default class FirstStartPage extends HIFullScreenView {
                 new VStack().id('viewer').grow(),
 
                 new HStack(
-                    new ClickButton(new TextView('Back')),
+                    new ClickButton(new TextView('Back')).whenClicked(() =>
+                        this.previous(),
+                    ),
                     new Spacer(),
-                    new ClickButton(new TextView('Next')),
+                    new ClickButton(new TextView('Next')).whenClicked(() =>
+                        this.next(),
+                    ),
                 )
                     .width('100%')
                     .padding()
@@ -45,11 +70,31 @@ export default class FirstStartPage extends HIFullScreenView {
 
         this.body.style.setProperty('-webkit-app-region', 'drag');
 
-        new ViewController({
+        this.controller = new ViewController({
             main: MainIntro(),
+            page1: Theming(),
         })
             .mapTo('FirstStartPageController')
             .bind(this.getViewById('viewer')!.body)
             .navigateTo();
+        this.pageNumber = 0;
+    }
+
+    public previous(): void {
+        if (this.pageNumber > 0) this.pageNumber -= 1;
+        this.updateController();
+    }
+
+    public next(): void {
+        this.pageNumber += 1;
+        this.updateController();
+    }
+
+    private updateController(): void {
+        if (this.pageNumber === 0) {
+            this.controller.navigateTo();
+        } else {
+            this.controller.navigateTo(`page${this.pageNumber}`);
+        }
     }
 }
