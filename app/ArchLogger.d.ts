cd502eeb527a6afe2bb442a01a8e2a9ca73775fa
Viraj Shah <shahvirajd@gmail.com>
2021-09-27 02:45:15 -0400

    Added declaration files for *.js source files

---
+++ app/ArchLogger.d.ts
@@ -0,0 +1,5 @@
+export function error(msg: string): number;
+export function warn(msg: string): number;
+export function info(msg: string): number;
+export function debug(msg: string): number;
+export function initializeLogger(): void;
