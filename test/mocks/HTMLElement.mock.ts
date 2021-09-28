4ee21f001200793c25d42e13a52e33a969e111bb
Viraj Shah <shahvirajd@gmail.com>
2021-09-28 11:51:56 -0400

    Implemented required mocking methods

--- test/mocks/HTMLElement.mock.ts
+++ test/mocks/HTMLElement.mock.ts
@@ -46,6 +46,44 @@ function queryFilterMap(tokenizedQuery: { type: string; value: string }[]): {
     };
 }
 
+export function assertIsHTMLElementMock(
+    val: unknown,
+): asserts val is HTMLElement {
+    if (typeof val === 'string') {
+        throw new Error(`${val} is not an HTMLElement`);
+    }
+
+    if (typeof val === 'object') {
+        if (
+            !(
+                Object.prototype.hasOwnProperty.call(val, 'tagName') &&
+                Object.prototype.hasOwnProperty.call(val, 'style') &&
+                Object.prototype.hasOwnProperty.call(val, 'classList') &&
+                Object.prototype.hasOwnProperty.call(val, 'id') &&
+                Object.prototype.hasOwnProperty.call(val, 'children')
+            )
+        ) {
+            throw new Error(
+                `${JSON.stringify(
+                    val,
+                    null,
+                    4,
+                )} is missing a property [tagName, style, classList, id, children]`,
+            );
+        }
+    }
+}
+
+export function assertIsHTMLElementMockCollection(
+    val: unknown,
+): asserts val is HTMLElementMock[] {
+    if (Object.prototype.isPrototypeOf.call(val, Array)) {
+        (val as Array<unknown>).forEach(element =>
+            assertIsHTMLElementMock(element),
+        );
+    }
+}
+
 export default class HTMLElementMock {
     public tagName: string;
 
@@ -55,12 +93,13 @@ export default class HTMLElementMock {
 
     public id: string;
 
-    private children: HTMLElementMock[];
+    private children: (HTMLElementMock | string)[];
 
     public constructor(tagName: string) {
         this.tagName = tagName;
         this.style = {};
         this.id = '';
+        this.children = [];
     }
 
     public querySelector(query: string): HTMLElementMock | null {
@@ -70,26 +109,45 @@ export default class HTMLElementMock {
     public querySelectorAll(query: string): HTMLElementMock[] {
         const queryMap = queryFilterMap(tokenizeQuery(query));
         let direct = this.children;
-        direct = queryMap.tagname
-            ? direct.filter(el => el.tagName === queryMap.tagname)
-            : direct;
-        direct = queryMap.id
-            ? direct.filter(el => el.id === queryMap.id)
-            : direct;
+        if (queryMap.tagname) {
+            direct = direct.filter(
+                el => typeof el !== 'string' && el.tagName === queryMap.tagname,
+            );
+        }
+
+        if (queryMap.id) {
+            direct.filter(
+                el => typeof el !== 'string' && el.id === queryMap.id,
+            );
+        }
+
         if (queryMap.classnames) {
             queryMap.classnames.forEach(className => {
                 direct = direct.filter(
-                    el => el.classList.indexOf(className) >= 0,
+                    el =>
+                        typeof el !== 'string' &&
+                        el.classList.indexOf(className) >= 0,
                 );
             });
         }
-        const childResults = this.children.flatMap(el =>
-            el.querySelectorAll(query),
-        );
+
+        const childResults = this.children.flatMap(el => {
+            if (typeof el !== 'string') return el.querySelectorAll(query);
+            return undefined;
+        });
+
         const out: HTMLElementMock[] = [];
+
+        assertIsHTMLElementMockCollection(direct);
+        assertIsHTMLElementMockCollection(childResults);
+
         direct.forEach(el => out.push(el));
         childResults.forEach(el => out.push(el));
         return out;
+    }
+
+    public append(...elements: (HTMLElementMock | 'string')[]): void {
+        elements.forEach(el => this.children.push(el));
     }
 
     public get className(): string {
