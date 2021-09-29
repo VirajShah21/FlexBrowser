2f2ae2811fea96e89061f8c948bcd34f0b209283
Viraj Shah <shahvirajd@gmail.com>
2021-09-29 19:59:10 -0400

    Refactored `DevKit` without Proxy objects

--- Client/ts/Components/DevKit.ts
+++ Client/ts/Components/DevKit.ts
@@ -1,78 +1,35 @@
 import { HColor, rgba } from '@Hi/Colors';
-import { StateObject } from '@Hi/Types/states';
 import View from '@Hi/View';
 import ClickButton from './ClickButton';
 import HStack from './HStack';
 import IonIcon from './IonIcon';
 import Spacer from './Spacer';
-import TextContent from './TextView';
+import TextView from './TextView';
 import VStack from './VStack';
 
 // ! Fix all errors with null/dnull and implement a workaround
-export class Preview extends VStack {
-    public readonly dimensions = StateObject(
-        {
-            width: 0,
-            height: 0,
-            padding: '',
-        },
-        property => {
-            if (property == 'width' || property == 'height')
-                (
-                    this.getViewById(`component-${property}`) as TextContent
-                ).model.text =
-                    (property == 'width'
-                        ? this.dimensions.width
-                        : this.dimensions.height) + '';
-            else if (property == 'padding')
-                (
-                    this.getViewById('component-padding') as TextContent
-                ).model.text = this.dimensions.padding || '•';
-        },
-    );
+export default class Preview extends VStack {
+    private readonly dimensions: {
+        width?: number;
+        height?: number;
+        padding?: string;
+    } = {
+        width: 0,
+        height: 0,
+        padding: '',
+    };
 
-    public readonly componentInfo = StateObject(
-        {
-            name: '',
-            id: '',
-            description: '',
-            padding: '',
-            margin: '',
-        },
-        property => {
-            switch (property) {
-                case 'name':
-                    (
-                        this.getViewById('component-name') as TextContent
-                    ).model.text = this.componentInfo.name || '•';
-                    break;
-                case 'id':
-                    (
-                        this.getViewById('component-id') as TextContent
-                    ).model.text = this.componentInfo.id || '•';
-                    break;
-                case 'description':
-                    (
-                        this.getViewById('component-description') as TextContent
-                    ).model.text = this.componentInfo.description || '•';
-                    break;
-            }
-        },
-    );
+    private readonly componentInfo: {
+        name?: string;
+        id?: string;
+        description?: string;
+    } = {
+        name: '',
+        id: '',
+        description: '',
+    };
 
-    public readonly viewerSettings = StateObject(
-        {
-            contrastToggle: false,
-        },
-        property => {
-            if (property == 'contrastToggle')
-                this.getViewById('toggle-contrast-button')?.foreground(
-                    HColor(
-                        this.viewerSettings.contrastToggle ? 'green' : 'gray',
-                    ),
-                );
-        },
-    );
+    private contrastToggle = false;
 
     constructor(content: View) {
         super(
@@ -81,8 +38,7 @@ export class Preview extends VStack {
                     'toggle-contrast-button',
                     'contrast-outline',
                 ).whenClicked(() => {
-                    this.viewerSettings.contrastToggle =
-                        !this.viewerSettings.contrastToggle;
+                    this.contrastToggle = !this.contrastToggle;
                 }),
             )
                 .rounded({
@@ -102,13 +58,13 @@ export class Preview extends VStack {
                     new Spacer(),
                     new HStack(
                         Preview.dimensionSub('width').padding(),
-                        new TextContent(' by '),
+                        new TextView(' by '),
                         Preview.dimensionSub('height').padding(),
                     ).id('component-dimensions'),
                     new Spacer(),
                     new VStack(
-                        new TextContent('•').id('component-padding').font('lg'),
-                        new TextContent('Padding')
+                        new TextView('•').id('component-padding').font('lg'),
+                        new TextView('Padding')
                             .font('sm')
                             .foreground(HColor('gray')),
                     )
@@ -118,22 +74,22 @@ export class Preview extends VStack {
                 ),
                 new HStack(
                     new VStack(
-                        new TextContent('•').id('component-name').font('lg'),
-                        new TextContent('Component')
+                        new TextView('•').id('component-name').font('lg'),
+                        new TextView('Component')
                             .font('sm')
                             .foreground(HColor('gray')),
                     ).padding(),
                     new VStack(
-                        new TextContent('•').id('component-id').font('lg'),
-                        new TextContent('ID')
+                        new TextView('•').id('component-id').font('lg'),
+                        new TextView('ID')
                             .font('sm')
                             .foreground(HColor('gray')),
                     ).padding(),
                 ),
-                new TextContent('Description')
+                new TextView('Description')
                     .font('sm')
                     .foreground(HColor('gray')),
-                new TextContent('•').id('component-description'),
+                new TextView('•').id('component-description'),
             ).padding(),
         );
 
@@ -141,7 +97,7 @@ export class Preview extends VStack {
     }
 
     override handle(data: string): void {
-        if (data == 'color') {
+        if (data === 'color') {
             this.getViewsByClass('preview-canvas').forEach(canvas =>
                 canvas.border({ color: HColor('gray5') }),
             );
@@ -149,6 +105,72 @@ export class Preview extends VStack {
                 wrapper.background(HColor('gray5')),
             );
         }
+    }
+
+    public get componentWidth(): number {
+        return this.dimensions.width;
+    }
+
+    public set componentWidth(val: number) {
+        this.dimensions.width = val;
+        (this.getViewById('component-width') as TextView).text = `${val}`;
+    }
+
+    public get componentHeight(): number {
+        return this.dimensions.height;
+    }
+
+    public set componentHeight(val: number) {
+        this.dimensions.height = val;
+        (this.getViewById('component-height') as TextView).text = `${val}`;
+    }
+
+    public get componentPadding(): string {
+        return this.dimensions.padding;
+    }
+
+    public set componentPadding(val: string) {
+        this.dimensions.padding = val;
+        (this.getViewById('component-padding') as TextView).text = `${val}`;
+    }
+
+    public get componentName(): string | undefined {
+        return this.componentInfo.name;
+    }
+
+    public set componentName(name: string | undefined) {
+        this.componentInfo.name = name;
+        (this.getViewById('component-name') as TextView).text = name || '•';
+    }
+
+    public get componentId(): string | undefined {
+        return this.componentInfo.id;
+    }
+
+    public set componentId(id: string | undefined) {
+        this.componentInfo.id = id;
+        (this.getViewById('component-id') as TextView).text = id || '•';
+    }
+
+    public get componentDescription(): string | undefined {
+        return this.componentInfo.description;
+    }
+
+    public set componentDescription(description: string | undefined) {
+        this.componentInfo.description = description;
+        (this.getViewById('component-description') as TextView).text =
+            description || '•';
+    }
+
+    public get contrastMode(): boolean {
+        return this.contrastToggle;
+    }
+
+    public set contrstMode(value: boolean) {
+        this.contrastToggle = value;
+        this.getViewById('toggle-contrast-button')?.foreground(
+            HColor(this.contrastToggle ? 'green' : 'gray'),
+        );
     }
 
     static enableHover(view: View, exampleViewer: Preview): void {
@@ -178,12 +200,12 @@ export class Preview extends VStack {
             else
                 exampleViewer.dimensions.padding = `${paddings[0]} ${paddings[1]} ${paddings[2]} ${paddings[3]}`;
 
-            if (exampleViewer.viewerSettings.contrastToggle)
+            if (exampleViewer.contrastToggle)
                 view.body.style.filter = 'brightness(50%)';
 
             ev.browserEvent.stopPropagation();
         }).whenMouseOut(() => {
-            if (exampleViewer.viewerSettings.contrastToggle)
+            if (exampleViewer.contrastToggle)
                 view.body.style.filter = 'brightness(100%)';
         });
 
@@ -194,8 +216,8 @@ export class Preview extends VStack {
 
     static dimensionSub(axis: 'width' | 'height'): VStack {
         return new VStack(
-            new TextContent('•').id(`component-${axis}`).font('lg'),
-            new TextContent(axis == 'width' ? 'Width' : 'Height')
+            new TextView('•').id(`component-${axis}`).font('lg'),
+            new TextView(axis == 'width' ? 'Width' : 'Height')
                 .font('sm')
                 .foreground(HColor('gray')),
         );
