4ae8c29630da0ff7afb1d2636fb132ce45768281
Viraj Shah <shahvirajd@gmail.com>
2021-09-29 11:42:12 -0400

    Filled test for hub trigger

--- test/views/triggers/hub-triggers.spec.ts
+++ test/views/triggers/hub-triggers.spec.ts
@@ -1,15 +1,31 @@
+import { ViewController } from '@Hi/ViewController';
 import { getAppController, reloadAppController } from '@UI/FlexBrowserApp';
+import { expect } from 'chai';
 import mockBrowser from '../../mocks/Browser.mock';
-
-mockBrowser();
+import HTMLElementMock from '../../mocks/HTMLElement.mock';
 
 describe('Hub (Trigger): Navigating to main hub page', () => {
+    let controller: ViewController;
+
     beforeEach(() => {
         mockBrowser();
         reloadAppController();
+        controller = getAppController();
     });
 
     it('Should navigate the controller back from the Windows Viewer', () => {
-        getAppController().navigateTo('windows');
+        controller.navigateTo('windows');
+        const windowsScreen = controller.screens[controller.visibleScreen];
+        const button = windowsScreen.getViewById('back-btn');
+        (button.body as unknown as HTMLElementMock).mockClick();
+        expect(controller.visibleScreen).to.equal('hub');
     });
+
+    // it('Should navigate the controller back from the Bookmarks Viewer', () => {
+    //     controller.navigateTo('windows');
+    //     const windowsScreen = controller.screens[controller.visibleScreen];
+    //     const button = windowsScreen.getViewById('back-btn');
+    //     (button.body as unknown as HTMLElementMock).mockClick();
+    //     expect(controller.visibleScreen).to.equal('hub');
+    // });
 });
