6249c2bb56b1f8eb9f165442c7d22e0c0d8274c4
Viraj Shah <shahvirajd@gmail.com>
2021-09-29 07:06:51 -0400

    More tests for browser window

--- test/FlexBrowserWindow.spec.ts
+++ test/FlexBrowserWindow.spec.ts
@@ -1,10 +1,15 @@
 import { expect } from 'chai';
 import DocumentMock from './mocks/Document.mock';
 import LocalStorageMock from './mocks/LocalStorage.mock';
+import FlexArch from './mocks/flexarch.mock';
 import FlexBrowserWindow from '../views/FlexBrowserWindow';
+import IonIcon from '@Hi/Components/IonIcon';
+import HTMLElementMock from './mocks/HTMLElement.mock';
+import InputField from '@Hi/Components/InputField';
 
 globalThis.document = DocumentMock as unknown as Document;
 globalThis.localStorage = LocalStorageMock as unknown as Storage;
+globalThis.flexarch = FlexArch;
 
 describe('FlexBrowserWindow resolving URL Input', () => {
     it('Should convert "abcd.ext" to "https://abcd.ext/"', () => {
@@ -45,7 +50,25 @@ describe('FlexBrowserWindow: URL Bar Navigation', () => {
         win = new FlexBrowserWindow();
     });
 
-    it('Should add to session history when second parameter is true', () => {
-        // expect(win)
+    it('Should add URL to session history if (and only if) param is true', () => {
+        expect(win.goTo('https://google.com/', false).length).to.equal(0);
+        expect(win.goTo('https://google.com/', true)).to.eql([
+            'https://google.com/',
+        ]);
+    });
+
+    it('Should change the "Go" icon to a "Reload icon"', () => {
+        const icon = win.getViewById('url-refresh-button') as IonIcon;
+        (icon.body as unknown as HTMLElementMock).name = 'alt-icon';
+        win.goTo('https://google.com/', false);
+        expect((icon.body as unknown as HTMLElementMock).name).to.equal(
+            'refresh-circle-outline',
+        );
+    });
+
+    it('Should update the URL Bar value', () => {
+        const urlbar = win.getViewById('url') as InputField;
+        win.goTo('https://virajshah.org/', false);
+        expect(urlbar.model.value).to.equal('https://virajshah.org/');
     });
 });
