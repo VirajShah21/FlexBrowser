db17f31878de4a48ccd219bb58a36c6777275aa9
Viraj Shah <shahvirajd@gmail.com>
2021-10-07 20:44:14 -0400

    Added tests for ValidURL

---
+++ test/views/Models/ValidURL.spec.ts
@@ -0,0 +1,46 @@
+import { describe } from 'mocha';
+import ValidURL from '@UI/Models/ValidURL';
+import { expect } from 'chai';
+
+describe('Models.ValidURL: When constructed with a URL query', () => {
+    let url1 = new ValidURL('google.com');
+    let url2 = new ValidURL('https://www.google.com');
+    let url3 = new ValidURL('http://google.com/');
+    let url4 = new ValidURL('https://www.google.com/search?q=Hello+World');
+    let url5 = new ValidURL('https://google.com/drive/something');
+    let url6 = new ValidURL('fake://google.com');
+
+    it('Should assume HTTPS protocol', () => {
+        expect(url1.protocol).to.equal('https');
+    });
+
+    it('Should retrieve the explicit protocol if one exists', () => {
+        expect(url2.protocol).to.equal('https');
+        expect(url3.protocol).to.equal('http');
+        expect(url4.protocol).to.equal('https');
+        expect(url5.protocol).to.equal('https');
+    });
+
+    it('Should throw an error when an unknown protocol is provided', () => {
+        expect(url6.protocol).to.throw(Error);
+    });
+
+    it('Should get the domain name without any trailing slashes', () => {
+        [url1, url3, url5].forEach(url =>
+            expect(url.domain).to.equal('google.com'),
+        );
+
+        [url2, url4].forEach(url =>
+            expect(url.domain).to.equal('www.google.com'),
+        );
+
+        expect(url6.domain).to.throw(Error);
+    });
+
+    it('Should get the path if provided (default is "/")', () => {
+        [url1, url2, url3].forEach(url => expect(url.path).to.equal('/'));
+        expect(url4.path).to.equal('/search?q=Hello+World');
+        expect(url5.path).to.equal('/dive/something');
+        expect(url6.path).to.throw(Error);
+    });
+});
---
+++ views/Models/ValidURL.ts
@@ -0,0 +1,57 @@
+type ValidURLProtocol = 'https' | 'http';
+
+export default class ValidURL {
+    private value: string;
+
+    constructor(value: string) {
+        if (typeof value === 'string') this.value = value.trim();
+    }
+
+    private isURL(): boolean {
+        if (this.value.includes(' ')) return false;
+        return true;
+    }
+
+    public get protocol(): ValidURLProtocol {
+        if (this.isURL()) {
+            if (this.value.includes('://')) {
+                const proto = this.value.substring(
+                    0,
+                    this.value.indexOf('://'),
+                );
+                if (proto === 'http' || proto === 'https') return proto;
+            }
+        }
+        return 'https';
+    }
+
+    public get domain(): string {
+        if (this.isURL()) {
+            const { protocol } = this;
+            let rest: string =
+                this.value.indexOf(protocol) === 0
+                    ? this.value.substring(protocol.length + 3)
+                    : this.value;
+            if (rest.includes('/')) rest = rest.substring(0, rest.indexOf('/'));
+            return rest;
+        }
+        return 'google.com';
+    }
+
+    public get path(): string {
+        if (this.isURL()) {
+            const { protocol } = this;
+            const rest: string =
+                this.value.indexOf(protocol) === 0
+                    ? this.value.substring(protocol.length + 3)
+                    : this.value;
+            if (rest.includes('/')) return rest.substring(rest.indexOf('/'));
+            return '/';
+        }
+        return `/search?q=${this.value.split(' ').join('+')}`;
+    }
+
+    public toString(): string {
+        return `${this.protocol}://${this.domain}${this.path}`;
+    }
+}
