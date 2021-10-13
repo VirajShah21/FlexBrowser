144eb7157f2f32fa14c3b002a19cb8dced645893
Viraj Shah <shahvirajd@gmail.com>
2021-10-13 19:58:51 -0400

    Added spaces in between each def in index.d.ts

--- app/index.d.ts
+++ app/index.d.ts
@@ -4,6 +4,9 @@ declare interface URLMeta {
 }
 
 export function readRC(): Record<string, unknown>;
+
 export function writeRC(data: Record<string, unknown>): void;
+
 export function readBookmarksFile(): URLMeta[];
+
 export function writeBookmarksFile(bookmarks: URLMeta[]): void;
