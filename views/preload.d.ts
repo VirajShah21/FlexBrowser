dc8fe7d7798e8ac642ce2bc122d692989a477596
Viraj Shah <shahvirajd@gmail.com>
2021-10-13 20:01:09 -0400

    Updated declarations for flexarch api

--- views/preload.d.ts
+++ views/preload.d.ts
@@ -3,10 +3,20 @@ declare interface URLMeta {
     url: string;
 }
 
+declare interface FlexRC {
+    colorTheme?: string;
+    theme?: string;
+}
+
 declare namespace flexarch {
     function changeUrl(to: string): void;
     function newWindow(): void;
     function getWindowList(): URLMeta[];
     function getBookmarks(): URLMeta[];
     function addBookmark(bookmark: URLMeta): void;
+    function pref(property: string, value?: string): string | null;
+    function getAllPreferences(): {
+        colorTheme?: string;
+        theme?: string;
+    };
 }
