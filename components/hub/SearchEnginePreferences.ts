3503f5e8ccd694b0f576c5d8fd6452df6e9210ba
Viraj Shah <shahvirajd@gmail.com>
2021-10-14 03:49:17 -0400

    Added search engine customization UI

--- components/hub/SearchEnginePreferences.ts
+++ components/hub/SearchEnginePreferences.ts
@@ -1,3 +1,6 @@
+import { HColor } from '@Hi/Colors';
+import Checkbox from '@Hi/Components/Checkbox';
+import ClickButton from '@Hi/Components/ClickButton';
 import HIFullScreenView from '@Hi/Components/HIFullScreenView';
 import HStack from '@Hi/Components/HStack';
 import ScrollView from '@Hi/Components/ScrollView';
@@ -10,7 +13,7 @@ export default class SearchEnginePreference extends HIFullScreenView {
         super(
             new VStack(
                 new HStack(
-                    new TextView('Theme')
+                    new TextView('Search Engine Preferences')
                         .font('md')
                         .bold()
                         .margin({ bottom: 10 }),
@@ -18,13 +21,41 @@ export default class SearchEnginePreference extends HIFullScreenView {
                 ).width('100%'),
 
                 new HStack(
-                    new TextView('Default'),
-                    new TextView('Name'),
-                    new TextView('URL Prefix'),
-                ),
+                    new TextView('').width('50px').textStart(),
+                    new TextView('Name')
+                        .width('calc((100% - 50px) / 2)')
+                        .textStart(),
+                    new TextView('URL Prefix')
+                        .width('calc((100% - 50px) / 2)')
+                        .textStart(),
+                )
+                    .width('100%')
+                    .background(HColor('background'))
+                    .rounded({ top: { left: 5, right: 5 } })
+                    .padding(5)
+                    .font('sm'),
 
-                new ScrollView(new TextView('Item 1'), new TextView('Item 2')),
-            ),
+                new ScrollView(
+                    new VStack(
+                        new HStack(
+                            new Spacer(),
+                            new Checkbox(),
+                            new Spacer(),
+                            new TextView('Google')
+                                .width('calc((100% - 50px) / 2')
+                                .textStart(),
+                            new TextView('https://google.com/search?q=')
+                                .width('calc((100% - 50px) / 2')
+                                .textStart(),
+                        )
+                            .width('100%')
+                            .font('sm'),
+                        new Spacer(),
+                    ).stretch(),
+                )
+                    .width('100%')
+                    .height('500px'),
+            ).padding(),
         );
     }
 }
