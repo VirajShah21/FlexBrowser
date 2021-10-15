d363a8728e2afbff91a286b0da142eaa8f18908b
Viraj Shah <shahvirajd@gmail.com>
2021-10-15 01:52:13 -0400

    Fix: Routing for first start page

--- views/FlexBrowserApp.ts
+++ views/FlexBrowserApp.ts
@@ -1,15 +1,29 @@
+import TextView from '@Hi/Components/TextView';
 import { ViewController } from '@Hi/ViewController';
+import FirstStartPage from './FirstStartPage';
 import FlexBrowserWindow from './FlexBrowserWindow';
 import FlexHub from './FlexHub/FlexHub';
 
 let AppController: ViewController;
 
 export function reloadAppController(flexWindow?: string): void {
-    AppController = new ViewController('AppController')
-        .bind()
-        .navigateTo(
-            flexWindow === 'browser' ? new FlexBrowserWindow() : new FlexHub(),
-        );
+    AppController = new ViewController('AppController').bind();
+
+    switch (flexWindow) {
+        case 'browser':
+            AppController.navigateTo(new FlexBrowserWindow());
+            break;
+        case 'hub':
+            AppController.navigateTo(new FlexHub());
+            break;
+        case 'firstStart':
+            AppController.navigateTo(new FirstStartPage());
+            break;
+        default:
+            AppController.navigateTo(
+                new TextView('Error loading window: Main process.'),
+            );
+    }
 }
 
 export function getAppController(): ViewController {
