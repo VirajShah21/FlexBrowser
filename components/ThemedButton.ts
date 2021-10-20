eeb69642a28dcf4705efff82e1aeec2d763d72ac
Viraj Shah <shahvirajd@gmail.com>
2021-10-20 03:28:05 -0400

    Made theme button component to replace default bluetext btn

---
+++ components/ThemedButton.ts
@@ -0,0 +1,11 @@
+import { HColor } from '@Hi/Colors';
+import ClickButton from '@Hi/Components/ClickButton';
+import View from '@Hi/View';
+import BrowserPreferences from '@Models/BrowserPreferences';
+
+export default class ThemedButton extends ClickButton {
+    constructor(...children: View[]) {
+        super(...children);
+        this.padding(0).foreground(HColor(BrowserPreferences.colorTheme));
+    }
+}
