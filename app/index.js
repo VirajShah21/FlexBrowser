7058806061df32ec62b7f06d08ac04e5058e3639
Viraj Shah <shahvirajd@gmail.com>
2021-10-21 10:53:06 -0400

    Added listener for in-page navigation

--- app/index.js
+++ app/index.js
@@ -167,6 +167,15 @@ function createWindow() {
         });
     });
     info('Binded Listener for resizing browser window');
+
+    win.getBrowserView().webContents.addListener(
+        'did-navigate-in-page',
+        event => {
+            const history = readHistoryFile();
+            history.push(win.getBrowserView().webContents.getURL());
+            writeHistoryFile(history);
+        },
+    );
 }
 
 /**
