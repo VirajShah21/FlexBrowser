93d82ba9d4a049e884ad34edd745e20f43c9a66b
Viraj Shah <shahvirajd@gmail.com>
2021-10-22 18:05:40 -0400

    Renamed to HideTaskbarButton

--- components/HideTaskbarButton.ts
+++ components/HideTaskbarButton.ts
@@ -1,7 +1,7 @@
 import IonIcon from '@Hi/Components/IonIcon';
 import TaskbarButton from './TaskbarButton';
 
-export default class ExpandTaskbarButton extends TaskbarButton {
+export default class HideTaskbarButton extends TaskbarButton {
     constructor() {
         super(new IonIcon('expand'));
         this.whenClicked(() => flexarch.focusHub());
