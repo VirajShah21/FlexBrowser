d3354733392059e39c1b666a1eabef4a280f6e58
Viraj Shah <shahvirajd@gmail.com>
2021-10-22 12:44:50 -0400

    Added glow to urlbar when focused

--- triggers/urlbar-triggers.ts
+++ triggers/urlbar-triggers.ts
@@ -3,6 +3,7 @@ import { HColor } from '@Hi/Colors';
 import InputField from '@Hi/Components/InputField';
 import IonIcon from '@Hi/Components/IonIcon';
 import TextField from '@Hi/Components/TextField';
+import RGBAModel from '@Hi/RGBAModel';
 import { defineTransition } from '@Hi/Transitions/Transition';
 import HumanEvent, { HumanKeyPressEvent } from '@Hi/Types/HumanEvent';
 import FlexBrowserWindow from '@UI/FlexBrowserWindow';
@@ -54,7 +55,9 @@ export function urlbarFocusedState(ev: HumanEvent): void {
     const urlField = ev.view as TextField;
     const urlBar = urlField.parent as URLBar;
     const reloadBtn = urlBar.findViewById('url-refresh-button') as IonIcon;
-    urlBar.transition(whenFocusedTransition);
+    urlBar.transition(whenFocusedTransition).then(() => {
+        urlBar.glow(RGBAModel.BLACK.alpha(0.25), 10);
+    });
     urlField.textStart().value = urlBar.urlInfo.url;
     (reloadBtn.body as HTMLInputElement).name = 'arrow-forward-outline';
 }
@@ -63,10 +66,12 @@ export function urlbarUnfocusedState(ev: HumanEvent): void {
     const urlField = ev.view as TextField;
     const urlBar = urlField.parent as URLBar;
     const reloadBtn = urlBar.findViewById('url-refresh-button') as IonIcon;
-    urlBar.transition(whenUnfocusedTransition);
+    urlBar.transition(whenUnfocusedTransition).then(() => {
+        urlBar.glow(RGBAModel.BLACK.alpha(0), 0);
+        urlField.placeholder = urlBar.urlInfo.title; // This can be delayed
+    });
     urlField.textCenter();
     urlField.value = '';
-    urlField.placeholder = urlBar.urlInfo.title;
     (reloadBtn.body as HTMLInputElement).name = 'reload';
 }
 
