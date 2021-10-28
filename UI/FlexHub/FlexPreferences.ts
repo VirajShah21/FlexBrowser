2523ec0bb4eada69ab43c0ee90133a27be5ce190
Viraj Shah <shahvirajd@gmail.com>
2021-10-28 12:57:53 -0400

    Adding preferences menu

--- UI/FlexHub/FlexPreferences.ts
+++ UI/FlexHub/FlexPreferences.ts
@@ -1,13 +1,30 @@
-import BrowserFramePreferences from '@Components/hub/BrowserFramePreferences';
-import HighlightColorPreferences from '@Components/hub/HighlightColorPreferences';
 import HubTitlebar from '@Components/hub/HubTitlebar';
-import SearchEnginePreferences from '@Components/hub/SearchEnginePreferences';
-import ThemePreferences from '@Components/hub/ThemePreferences';
 import { HColor } from '@Hi/Colors';
+import ClickButton from '@Hi/Components/ClickButton';
 import HIFullScreenView from '@Hi/Components/HIFullScreenView';
+import HStack from '@Hi/Components/HStack';
 import ScrollView from '@Hi/Components/ScrollView';
+import TextView, { FontWeight } from '@Hi/Components/TextView';
 import VStack from '@Hi/Components/VStack';
+import Resources from '@Hi/Resources';
 import HubTitles from '@Resources/strings/HubTitles.json';
+
+export class FlexPreferenceMenuButton extends ClickButton {
+    public constructor(public readonly name: string) {
+        super(
+            new VStack(
+                Resources.getImageView(`PreferenceIcons/${name}.png`)
+                    .height(64)
+                    .width(64),
+                new TextView(name)
+                    .weight(FontWeight.Medium)
+                    .foreground(HColor('gray')),
+            ),
+        );
+
+        this.width(100).height(100);
+    }
+}
 
 /**
  * The main preferences pane in the hub.
@@ -28,13 +45,41 @@ export default class FlexPreferences extends HIFullScreenView {
                 new HubTitlebar(HubTitles.Preferences).insertBackButton(true),
 
                 new ScrollView(
-                    new HighlightColorPreferences(),
+                    // new HighlightColorPreferences(),
 
-                    new ThemePreferences(),
+                    // new ThemePreferences(),
 
-                    new BrowserFramePreferences(),
+                    // new BrowserFramePreferences(),
 
-                    new SearchEnginePreferences(),
+                    // new SearchEnginePreferences(),
+
+                    new VStack(
+                        new TextView('Styles')
+                            .font('md')
+                            .bold()
+                            .margin({ bottom: 10 })
+                            .width('100%')
+                            .textStart()
+                            .foreground(HColor('gray')),
+                        new HStack(
+                            new FlexPreferenceMenuButton('Colors'),
+                            new FlexPreferenceMenuButton('Browser Frame'),
+                        )
+                            .alignStart()
+                            .width('100%'),
+                        new TextView('Search & Navigation')
+                            .font('md')
+                            .bold()
+                            .margin({ bottom: 10 })
+                            .width('100%')
+                            .textStart()
+                            .foreground(HColor('gray')),
+                        new HStack(
+                            new FlexPreferenceMenuButton('Search Engines'),
+                        )
+                            .alignStart()
+                            .width('100%'),
+                    ),
                 )
                     .height('100%')
                     .width('100%')
