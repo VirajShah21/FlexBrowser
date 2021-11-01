d0082e041c369649347de074a2125ecb65a74d98
Viraj Shah <shahvirajd@gmail.com>
2021-11-01 13:15:07 -0400

    Added title to distringuish bookmarks from open tabs

--- UI/FlexHub/FlexBookmarksViewer.ts
+++ UI/FlexHub/FlexBookmarksViewer.ts
@@ -8,7 +8,7 @@ import HStack from '@Hi/Components/HStack';
 import IonIcon from '@Hi/Components/IonIcon';
 import ScrollView from '@Hi/Components/ScrollView';
 import Spacer from '@Hi/Components/Spacer';
-import TextView from '@Hi/Components/TextView';
+import TextView, { FontWeight } from '@Hi/Components/TextView';
 import VStack from '@Hi/Components/VStack';
 import BookmarksManager from '@Models/BookmarksManager';
 import ValidURL from '@Models/ValidURL';
@@ -25,7 +25,11 @@ export default class FlexBookmarksViewer extends HIFullScreenView {
                 new ScrollView(
                     new VStack(
                         new DetailsView(
-                            new DetailsSummaryView(new TextView('Open Windows'))
+                            new DetailsSummaryView(
+                                new TextView('Open Windows').weight(
+                                    FontWeight.Bold,
+                                ),
+                            )
                                 .textStart()
                                 .foreground(HColor('gray')),
                             ...flexarch.getWindowList().map(
@@ -39,28 +43,17 @@ export default class FlexBookmarksViewer extends HIFullScreenView {
                                                 left: 25,
                                             }),
                                             new Spacer(),
-
-                                            // new ClickButton(
-                                            //     new IonIcon('bookmark')
-                                            //         .whenMouseOver(ev => {
-                                            //             // eslint-disable-next-line no-param-reassign
-                                            //             (ev.view as IonIcon).name =
-                                            //                 'trash-outline';
-                                            //         })
-                                            //         .whenMouseOut(ev => {
-                                            //             // eslint-disable-next-line no-param-reassign
-                                            //             (ev.view as IonIcon).name =
-                                            //                 'bookmark';
-                                            //         }),
-                                            // ).whenClicked(() => {
-                                            //     BookmarksManager.removeBookmark(
-                                            //         new ValidURL(meta.url),
-                                            //     );
-                                            // }),
                                         ).stretch(),
                                     ),
                             ),
                         ).width('100%'),
+
+                        new TextView('Bookmarks')
+                            .weight(FontWeight.Bold)
+                            .margin({ top: 20 })
+                            .width('100%')
+                            .textStart()
+                            .foreground(HColor('gray')),
 
                         ...flexarch.getBookmarks().map(bookmark =>
                             new ClickButton(
@@ -95,7 +88,8 @@ export default class FlexBookmarksViewer extends HIFullScreenView {
                         ),
                     )
                         .stretch()
-                        .padding(),
+                        .padding()
+                        .alignStart(),
                 ).stretch(),
             )
                 .stretch()
