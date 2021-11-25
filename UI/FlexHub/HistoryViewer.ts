313f3a22633bab9ab46ecfa4c66d1bbb6378c841
Viraj Shah <shahvirajd@gmail.com>
2021-11-25 04:16:43 -0500

    Implemented parser for reading history records

---
+++ UI/FlexHub/HistoryViewer.ts
@@ -0,0 +1,85 @@
+import BaseHubWindow from './BaseHubWindow';
+
+type DayOfWeek = 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat';
+type Month =
+    | 'Jan'
+    | 'Feb'
+    | 'Mar'
+    | 'Apr'
+    | 'May'
+    | 'Jun'
+    | 'Jul'
+    | 'Aug'
+    | 'Sep'
+    | 'Oct'
+    | 'Nov'
+    | 'Dec';
+
+interface HistoryRecord {
+    date: {
+        day: DayOfWeek;
+        month: Month;
+        date: number;
+        year: number;
+    };
+    time: {
+        h: number;
+        m: number;
+        s: number;
+    };
+    url: string;
+    title: string;
+}
+
+export default class HistoryViewer extends BaseHubWindow {
+    constructor() {
+        super('History');
+    }
+
+    private static parseHistoryRecord(record: string): HistoryRecord {
+        const split = record.split(' ');
+        const day = split[0] as DayOfWeek;
+        const month = split[1] as Month;
+        const date = parseInt(split[2] as string, 10);
+        const year = parseInt(split[3] as string, 10);
+        const time = split[4] as string;
+
+        const protocolSlashStart = record.indexOf('://');
+        let urlStart = 0; // just a default value to avoid TS errors
+        let urlEnd = 0; // just a default value to avoid TS errors
+
+        for (let i = protocolSlashStart; i >= 0; i -= 1) {
+            if (record[i] === ' ') {
+                urlStart = i + 1;
+                break;
+            }
+        }
+
+        for (let i = urlStart; i < record.length; i += 1) {
+            if (record[i] === ' ') {
+                urlEnd = i;
+                break;
+            }
+        }
+
+        const url = record.substring(urlStart, urlEnd);
+
+        const title = record.substring(urlEnd + 1);
+
+        return {
+            date: {
+                day,
+                month,
+                year,
+                date,
+            },
+            time: {
+                h: parseInt(time.split(':')[0] as string, 10),
+                m: parseInt(time.split(':')[1] as string, 10),
+                s: parseInt(time.split(':')[2] as string, 10),
+            },
+            url,
+            title,
+        };
+    }
+}
