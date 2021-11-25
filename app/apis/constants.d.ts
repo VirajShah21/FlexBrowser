d24d211c34ac83ad8222acc305ff93a503bac447
Viraj Shah <shahvirajd@gmail.com>
2021-11-25 17:16:52 -0500

    Fixed a minor error with type declarations file

--- app/apis/constants.d.ts
+++ app/apis/constants.d.ts
@@ -1,3 +1,3 @@
-declare const DEFAULT_WINDOW_WIDTH: number;
-declare const DEFAULT_WINDOW_HEIGHT: number;
-declare const TOP_FRAME_HEIGHT: number;
+export const DEFAULT_WINDOW_WIDTH: number;
+export const DEFAULT_WINDOW_HEIGHT: number;
+export const TOP_FRAME_HEIGHT: number;
