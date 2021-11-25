fb5f9c103255adc48866a918563935822f910294
Viraj Shah <shahvirajd@gmail.com>
2021-11-25 03:17:32 -0500

    Fixed reload issue

--- components/RefreshTaskbarButton.ts
+++ components/RefreshTaskbarButton.ts
@@ -1,9 +1,7 @@
 import { HColor } from '@Hi/Colors';
 import ClickButton from '@Hi/Components/ClickButton';
-import InputField from '@Hi/Components/InputField';
 import IonIcon from '@Hi/Components/IonIcon';
 import BrowserPreferences from '@Models/BrowserPreferences';
-import FlexBrowserWindow from '@UI/FlexBrowserWindow';
 
 export default class RefreshURLButton extends ClickButton {
     constructor() {
@@ -11,15 +9,7 @@ export default class RefreshURLButton extends ClickButton {
         this.padding(0)
             .font('md')
             .foreground(HColor('gray'))
-            .whenClicked(ev => {
-                const browserWindow = ev.view.root(
-                    view => (view as FlexBrowserWindow).isBrowserWindow,
-                ) as FlexBrowserWindow;
-                const url = (browserWindow.findViewById('url') as InputField)
-                    .value;
-
-                browserWindow.goTo(url);
-            })
+            .whenClicked(() => flexarch.reloadBrowserView())
             .whenMouseOver(ev =>
                 ev.view.foreground(HColor(BrowserPreferences.colorTheme)),
             )
