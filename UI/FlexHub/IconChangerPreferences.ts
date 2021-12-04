b7ca2bc7982ca481c057bc97061099086c1647e7
Viraj Shah <shahvirajd@gmail.com>
2021-12-04 05:59:56 -0500

    Reorganized items

--- UI/FlexHub/IconChangerPreferences.ts
+++ UI/FlexHub/IconChangerPreferences.ts
@@ -1,11 +1,13 @@
 import SectionTitle from '@Components/SectionTitle';
 import ThemedButton from '@Components/ThemedButton';
+import { HColor } from '@Hi/Colors';
 import HStack from '@Hi/Components/HStack';
 import IonIcon from '@Hi/Components/IonIcon';
 import ScrollView from '@Hi/Components/ScrollView';
 import VStack from '@Hi/Components/VStack';
 import RGBAModel from '@Hi/RGBAModel';
 import View from '@Hi/View';
+import BrowserPreferences from '@Models/BrowserPreferences';
 import BaseHubWindow from './BaseHubWindow';
 
 export class IconButton extends ThemedButton {
@@ -16,6 +18,13 @@ export class IconButton extends ThemedButton {
             .rounded()
             .background(RGBAModel.WHITE)
             .margin(10);
+    }
+
+    public activate(): this {
+        this.background(HColor(BrowserPreferences.colorTheme)).foreground(
+            HColor('background'),
+        );
+        return this;
     }
 }
 
@@ -29,6 +38,16 @@ export default class IconChangerPreferences extends BaseHubWindow {
 
                     new HStack(
                         new IconButton(
+                            new IonIcon('chevron-back'),
+                            new IonIcon('chevron-forward'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('chevron-back-circle-outline'),
+                            new IonIcon('chevron-forward-circle-outline'),
+                        ),
+
+                        new IconButton(
                             new IonIcon('arrow-back'),
                             new IonIcon('arrow-forward'),
                         ),
@@ -36,6 +55,40 @@ export default class IconChangerPreferences extends BaseHubWindow {
                         new IconButton(
                             new IonIcon('arrow-back-circle-outline'),
                             new IonIcon('arrow-forward-circle-outline'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('caret-back-circle-outline'),
+                            new IonIcon('caret-forward-circle-outline'),
+                        ),
+                    )
+                        .width('100%')
+                        .alignStart(),
+
+                    new HStack(
+                        new IconButton(
+                            new IonIcon('return-down-back'),
+                            new IonIcon('return-up-back'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('chevron-back-circle'),
+                            new IonIcon('chevron-forward-circle'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('arrow-back-circle'),
+                            new IonIcon('arrow-forward-circle'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('caret-back-circle'),
+                            new IonIcon('caret-forward-circle'),
+                        ),
+
+                        new IconButton(
+                            new IonIcon('caret-back'),
+                            new IonIcon('caret-forward'),
                         ),
                     )
                         .width('100%')
