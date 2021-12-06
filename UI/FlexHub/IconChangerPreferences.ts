0acc2d370ce942566b25a380385750f284017135
Viraj Shah <shahvirajd@gmail.com>
2021-12-05 22:55:16 -0500

    Created interactive UI for changing icons

--- UI/FlexHub/IconChangerPreferences.ts
+++ UI/FlexHub/IconChangerPreferences.ts
@@ -6,25 +6,54 @@ import IonIcon from '@Hi/Components/IonIcon';
 import ScrollView from '@Hi/Components/ScrollView';
 import VStack from '@Hi/Components/VStack';
 import RGBAModel from '@Hi/RGBAModel';
+import HumanEvent from '@Hi/Types/HumanEvent';
 import View from '@Hi/View';
 import BrowserPreferences from '@Models/BrowserPreferences';
 import BaseHubWindow from './BaseHubWindow';
 
 export class IconButton extends ThemedButton {
-    constructor(...children: View[]) {
+    private themeName: string;
+
+    private iconType: string;
+
+    private static instances: IconButton[] = [];
+
+    constructor(iconType: string, themeName: string, ...children: View[]) {
         super(new HStack(...children));
         this.font('xl')
             .padding()
             .rounded()
             .background(RGBAModel.WHITE)
-            .margin(10);
+            .margin(10)
+            .whenClicked(IconButton.setIconTheme);
+        this.themeName = themeName;
+        this.iconType = iconType;
+        IconButton.instances.push(this);
     }
 
     public activate(): this {
+        IconButton.instances.forEach(instance => {
+            if (instance && instance.iconType === this.iconType) {
+                instance
+                    .background(HColor('background'))
+                    .foreground(HColor(BrowserPreferences.ColorTheme));
+            }
+        });
         this.background(HColor(BrowserPreferences.ColorTheme)).foreground(
             HColor('background'),
         );
         return this;
+    }
+
+    public static setIconTheme(ev: HumanEvent): void {
+        const instance = ev.view as IconButton;
+        const iconThemes = BrowserPreferences.IconTheme;
+        Object.defineProperty(iconThemes, instance.iconType, {
+            value: instance.themeName,
+            writable: true,
+        });
+        BrowserPreferences.IconTheme = iconThemes;
+        instance.activate();
     }
 }
 
@@ -38,26 +67,36 @@ export default class IconChangerPreferences extends BaseHubWindow {
 
                     new HStack(
                         new IconButton(
+                            'backForward',
+                            'chevron',
                             new IonIcon('chevron-back'),
                             new IonIcon('chevron-forward'),
                         ).activate(),
 
                         new IconButton(
+                            'backForward',
+                            'chevron-circle-outline',
                             new IonIcon('chevron-back-circle-outline'),
                             new IonIcon('chevron-forward-circle-outline'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'arrow',
                             new IonIcon('arrow-back'),
                             new IonIcon('arrow-forward'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'arrow-circle-outline',
                             new IonIcon('arrow-back-circle-outline'),
                             new IonIcon('arrow-forward-circle-outline'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'caret-circle-outline',
                             new IonIcon('caret-back-circle-outline'),
                             new IonIcon('caret-forward-circle-outline'),
                         ),
@@ -67,26 +106,36 @@ export default class IconChangerPreferences extends BaseHubWindow {
 
                     new HStack(
                         new IconButton(
+                            'backForward',
+                            'return',
                             new IonIcon('return-down-back'),
                             new IonIcon('return-up-forward'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'chevron-circle-filled',
                             new IonIcon('chevron-back-circle'),
                             new IonIcon('chevron-forward-circle'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'arrow-circle-filled',
                             new IonIcon('arrow-back-circle'),
                             new IonIcon('arrow-forward-circle'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'caret-circle-filled',
                             new IonIcon('caret-back-circle'),
                             new IonIcon('caret-forward-circle'),
                         ),
 
                         new IconButton(
+                            'backForward',
+                            'caret',
                             new IonIcon('caret-back'),
                             new IonIcon('caret-forward'),
                         ),
@@ -98,18 +147,40 @@ export default class IconChangerPreferences extends BaseHubWindow {
 
                     new HStack(
                         new IconButton(
+                            'reload',
+                            'default',
                             new IonIcon('reload-outline'),
                         ).activate(),
-                        new IconButton(new IonIcon('reload-circle-outline')),
-                        new IconButton(new IonIcon('reload-circle')),
+                        new IconButton(
+                            'reload',
+                            'default-circle-outline',
+                            new IonIcon('reload-circle-outline'),
+                        ),
+                        new IconButton(
+                            'reload',
+                            'default-circle-filled',
+                            new IonIcon('reload-circle'),
+                        ),
                     )
                         .width('100%')
                         .alignStart(),
 
                     new HStack(
-                        new IconButton(new IonIcon('refresh')),
-                        new IconButton(new IonIcon('refresh-circle-outline')),
-                        new IconButton(new IonIcon('refresh-circle')),
+                        new IconButton(
+                            'reload',
+                            'alternative',
+                            new IonIcon('refresh'),
+                        ),
+                        new IconButton(
+                            'reload',
+                            'alternative-circle-outline',
+                            new IonIcon('refresh-circle-outline'),
+                        ),
+                        new IconButton(
+                            'reload',
+                            'alternative-circle-filled',
+                            new IonIcon('refresh-circle'),
+                        ),
                     )
                         .width('100%')
                         .alignStart(),
@@ -117,19 +188,51 @@ export default class IconChangerPreferences extends BaseHubWindow {
                     new SectionTitle('Hub Icon'),
 
                     new HStack(
-                        new IconButton(new IonIcon('home-outline')).activate(),
-                        new IconButton(new IonIcon('apps-outline')),
-                        new IconButton(new IonIcon('grid-outline')),
-                        new IconButton(new IonIcon('rocket-outline')),
+                        new IconButton(
+                            'hub',
+                            'home-outline',
+                            new IonIcon('home-outline'),
+                        ).activate(),
+                        new IconButton(
+                            'hub',
+                            'apps-outline',
+                            new IonIcon('apps-outline'),
+                        ),
+                        new IconButton(
+                            'hub',
+                            'grid-outline',
+                            new IonIcon('grid-outline'),
+                        ),
+                        new IconButton(
+                            'hub',
+                            'rocket-outline',
+                            new IonIcon('rocket-outline'),
+                        ),
                     )
                         .width('100%')
                         .alignStart(),
 
                     new HStack(
-                        new IconButton(new IonIcon('home')),
-                        new IconButton(new IonIcon('apps')),
-                        new IconButton(new IonIcon('grid')),
-                        new IconButton(new IonIcon('rocket')),
+                        new IconButton(
+                            'hub',
+                            'home-filled',
+                            new IonIcon('home'),
+                        ),
+                        new IconButton(
+                            'hub',
+                            'apps-filled',
+                            new IonIcon('apps'),
+                        ),
+                        new IconButton(
+                            'hub',
+                            'grid-filled',
+                            new IonIcon('grid'),
+                        ),
+                        new IconButton(
+                            'hub',
+                            'rocket-filled',
+                            new IonIcon('rocket'),
+                        ),
                     )
                         .width('100%')
                         .alignStart(),
