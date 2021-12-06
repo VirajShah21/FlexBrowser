bcd9b1c2a0ae497d1c76352f2087305c56ffaeea
Viraj Shah <shahvirajd@gmail.com>
2021-12-06 02:33:05 -0500

    Documented `Favicon` class

--- components/Favicon.ts
+++ components/Favicon.ts
@@ -1,7 +1,20 @@
 import ImageView from '@Hi/Components/ImageView';
 import ValidURL from '@Models/ValidURL';
 
+/**
+ * A URL-based favicon loader.
+ *
+ * @export
+ * @class Favicon
+ * @extends {ImageView}
+ */
 export default class Favicon extends ImageView {
+    /**
+     * Creates an instance of Favicon.
+     * @param {ValidURL} url The URL for which to load the favicon.
+     *
+     * @memberOf Favicon
+     */
     constructor(url: ValidURL) {
         super(Favicon.getFaviconURL(url));
         this.rounded('100%').width(36).height(36);
@@ -17,6 +30,16 @@ export default class Favicon extends ImageView {
         });
     }
 
+    /**
+     * Builds a potential favicon URL.
+     *
+     * @static
+     * @param {ValidURL} url The URL for which to load the favicon.
+     * @param {string} [extension='ico'] The favicon image file extension.
+     * @returns {string} The built favicon URL as a string.
+     *
+     * @memberOf Favicon
+     */
     public static getFaviconURL(url: ValidURL, extension = 'ico'): string {
         return `${url.protocol}://${url.domain}/favicon.${extension}`;
     }
